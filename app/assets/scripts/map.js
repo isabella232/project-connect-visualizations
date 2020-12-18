d2b2ac2a637b8079ad4b99acf68e61494e043caa
Rub21 <rub2106@gmail.com>
2020-12-18 11:29:15 -0500

    first commit

---
+++ app/assets/scripts/map.js
@@ -0,0 +1,21 @@
+
+// console.log('------------------------------------');
+// console.log('start');
+// console.log('------------------------------------');
+// mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q';
+
+// var beforeMap = new mapboxgl.Map({
+//     container: 'before',
+//     style: 'mapbox://styles/devseed/ckirgvv9r06sb17p3d4ndr2r3',
+//     center: [29.8812, -1.9180],
+//     zoom: 8.3
+// });
+// var afterMap = new mapboxgl.Map({
+//     container: 'after',
+//     style: 'mapbox://styles/devseed/ckis8k38i0pjt19nnlm1hhnso',
+//     center: [29.8812, -1.9180],
+//     zoom: 8.3
+// });
+// var map = new mapboxgl.Compare(beforeMap, afterMap, {
+//     mousemove: true
+// });
